d7bfa108946d7b185c399e2db881f062a07f45bc
cudak <jsbzwork@gmail.com>
2016-06-03 22:42:33 +0900

    camerazoom and check obj file

---
+++ MAGNI/Assets/src/CameraZoom.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKey("space")) {
+            Camera.main.orthographicSize = (float)(0.98) * Camera.main.orthographicSize;
+        }
+	}
+}
