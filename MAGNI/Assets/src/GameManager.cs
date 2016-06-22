0f02a1907debff74964149f99a6e2a0f55ec06bc
cudak <jsbzwork@gmail.com>
2016-06-23 01:13:28 +0900

    gamemanager code added

---
+++ MAGNI/Assets/src/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    
+    static public bool[] scoreQueue;
+    static private int size;
+    static private int queuePoint;
+
+	// Use this for initialization
+	void Start () {
+        size = 20;
+        scoreQueue = new bool[size];
+        for(int i = 0; i < size;i++)
+        {
+            scoreQueue[i] = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    int getQPoint()
+    {
+        int i = queuePoint % size;
+        queuePoint++;
+        return i;
+    }
+
+    public void writeScore(bool b)
+    {
+        scoreQueue[getQPoint()] = b;
+    }
+
+    public int getScore()
+    {
+        int sum = 0;
+        for(int i = 0; i < size;i++)
+        {
+            if (scoreQueue[i]) sum++;
+        }
+        return sum;
+    }
+}
--- MAGNI/Assets/src/RevertCamera.cs
+++ MAGNI/Assets/src/RevertCamera.cs
@@ -8,19 +8,31 @@ public class RevertCamera : MonoBehaviour {
 
     GameObject objInit;
     ObjectInit objIT;
+    GameManager gm;
 
     void Start () {
         objInit = GameObject.Find("ObjectManager");
         objIT = objInit.GetComponent<ObjectInit>();
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 	
 	void Update () {
         //if Camera is zoomed to threshold
         if(Camera.main.orthographicSize < threshold)
         {
+            // Scoring
+            foreach (Transform child in GameObject.Find("SmallOne0").transform)
+            {
+                gm.writeScore(child.eulerAngles.y < 200.0f && child.eulerAngles.y > 160.0f);
+                break;
+            }
+                
             // Camera revert
             Camera.main.orthographicSize = initialCameraSize;
+
+            // Object Init
             objIT.InitNew();
+
         }
     }
 
